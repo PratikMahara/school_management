979aede788a6a6b5285fa423845f76b39cd29f7f
Prabesh Bhandari <prabeshbhandari842@gmail.com>
2025-06-12 21:47:05 +0545

    feat: leave application by students form and route

---
+++ frontend/src/student/components/leave-application/LeaveApplication.jsx
@@ -0,0 +1,9 @@
+import LeaveApplicationForm from "@/components/leave-application-form/LeaveApplicationForm"
+
+export default function LeaveApplication() {
+  return (
+    <div className="space-y-6">
+        <LeaveApplicationForm />
+    </div>
+  )
+}
